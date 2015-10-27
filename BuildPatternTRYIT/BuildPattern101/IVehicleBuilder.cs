5b1b62d274b6f51a8c1893eb347cb12785d0e214
Danny Jeffery <daniel.jeffery@comparethemarket.com>
2015-10-27 15:47:36 +0000

    Updated starter code

--- BuildPatternTRYIT/BuildPattern101/IVehicleBuilder.cs
+++ BuildPatternTRYIT/BuildPattern101/IVehicleBuilder.cs
@@ -1,7 +1,13 @@
 ﻿namespace BuildPattern101
 {
+    // THE BUILDER INTERFACE
     public interface IVehicleBuilder
     {
-        // TODO : Create the required methods for the builder
+        VehicleBuilder WithNumberOfSeats(int numberOfSeats);
+        VehicleBuilder WithEngineSize(int engineSize);
+        VehicleBuilder WithColour(string colour);
+        VehicleBuilder WithBootType(string bootType);
+        VehicleBuilder WithNumberOfWheels(int numberOfWheels);
+        Vehicle BuildVehicle();
     }
 }
